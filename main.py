139c1c6b78184c0572d0ae814e94bdc7f8f9ce93
Mohammad Hossein Ahmadi <mh.ahmadi258.work@gmail.com>
2021-07-11 01:44:59 +0430

    improve classification performance

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ M = 700
 output_size = (N,M)
 
 # this method prepare image size for model input
-def prepare_image(img):
-  img = cv2.resize(img, dsize=(36,58))
-  img = np.expand_dims(img, axis=0)
-  return img
+def prepare_image(imgs):
+    imgs =np.array([cv2.resize(img, dsize=(36,58)) for img in imgs])
+    return imgs
 
 # Create I shape closing kernel
 closing_kernel = np.zeros((25,15),dtype=np.uint8)
@@ -73,32 +72,41 @@ while True:
 
     pts = pts[1:]   # remove background component point
 
-# Insert Circle to the images
-    for pt, hw, box in zip(pts,hws,boxes):
-        if abs(hw[0] - hw[1]) > 10 :    # remove the  ball
-            croped_image = I[box[1]:box[3],box[0]:box[2],:]
-            # Here Insert classification code
-            input_image = prepare_image(croped_image)
-            softmax = model.predict(input_image)
-            label = np.argmax(softmax)
+    # compute croped images
+    images = list()
+    for box,hw in zip(boxes,hws):
+        croped_image = I[box[1]:box[3],box[0]:box[2],:]
+        images.append(croped_image)
 
-            if label == 0:
-                cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(255,0,0),-1)
-            elif label == 1:
-                cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(255,255,255),-1)
-            elif label == 2:
-                cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(0,255,255),-1)
-            else :
-                cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(0,0,255),-1)
+    # compute labales
+    images = prepare_image(images)
+    labels = np.array([])
+    if len(images) > 0:
+        softmaxs = model.predict(images)
+        labels = np.argmax(softmaxs,axis=1)
 
-            cv2.rectangle(I, (box[:2]), (box[2:]), (0, 255, 0), 2)
-            cv2.circle(J,(int(pt[0]),int(pt[1])),5,(0,0,255))
+    # add circle to output image
+    for pt, label, hw, box in zip(pts,labels,hws,boxes):
+        if abs(hw[0] - hw[1]) <= 10 :    # remove the  ball
+            continue
+        if label == 0:
+            cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(255,0,0),-1)   # Blue
+        elif label == 1:
+            cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(255,255,255),-1)   # White
+        elif label == 2:
+            cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(0,255,255),-1) # Yellow
+        else :
+            cv2.circle(out_image,(int(pt[0]),int(pt[1])),15,(0,0,255),-1)   # Red
+        cv2.rectangle(I, (box[:2]), (box[2:]), (0, 255, 0), 2)
+        cv2.circle(J,(int(pt[0]),int(pt[1])),5,(0,0,255))
+
+    
 
 # Display images 
-    # cv2.imshow('I',I)
-    # cv2.imshow('j',J)
+    cv2.imshow('I',I)
+    cv2.imshow('j',J)
     cv2.imshow('out',out_image)
-    # cv2.imshow('FG Mask',fgmask)
+    cv2.imshow('FG Mask',fgmask)
     
 
     key = cv2.waitKey(33)   # ~ 30 frames per second
